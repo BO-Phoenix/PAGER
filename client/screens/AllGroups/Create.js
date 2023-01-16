632eaf5d60b6acbf492ce87b6fede45c2d85edf0
Madeline King <madeline.king330@gmail.com>
2023-01-16 15:05:38 -0800

    add top nav tabs to user-groups

--- client/screens/AllGroups/Create.js
+++ client/screens/AllGroups/Create.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Form, FormItem, Picker } from 'react-native-form-component';
 
 const styles = StyleSheet.create({
@@ -40,10 +40,9 @@ const Create = () => {
   const [vibe, setVibe] = useState('');
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       {/* <Text>I Am Create</Text> */}
       <StatusBar style="auto" />
-
       <Text id="create-group" style={styles.title}>CREATE GROUP</Text>
       <Form
         onButtonPress={() => console.warn('do something')}
@@ -91,28 +90,7 @@ const Create = () => {
           isRequired
         />
       </Form>
-        {/* <form id="create-group-form" onSubmit={handleSubmit}>
-          <input id="group-name" type="text" placeholder="GROUP NAME" name="group-name" />
-
-          <input id="group-description" type="text" placeholder="GROUP DESCRIPTION" name="group-description" />
-
-          <select id="select-size" name="select-size" >
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-          </select>
-
-          <select id="select-vibe" name="select-vibe" >
-            <option value="small">Low</option>
-            <option value="medium">Medium</option>
-            <option value="large">High</option>
-          </select>
-
-          <input id="upload-photo" type="text" name="upload-photo" />
-
-          <button className="create-group-btn" type="submit">CREATE GROUP</button>
-        </form> */}
-    </View>
+    </SafeAreaView>
   );
 };
 
--- client/screens/AllGroups/Index.js
+++ client/screens/AllGroups/Index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import globalStyles from '../../globalStyles';
 import Upcoming from './Upcoming.js';
 import Attended from './Attended.js';
@@ -12,47 +13,67 @@ const Index = () => {
 
   const Stack = createNativeStackNavigator();
 
-  // const Tab = createBottomTabNavigator();
+  const Tab = createMaterialTopTabNavigator();
 
   return (
     // <View style={globalStyles.container}>
     //   <Text style={globalStyles.text}>_______ Screen</Text>
     // </View>
 
-      <Stack.Navigator initialRouteName="Attended">
-        <Stack.Screen
-          name="Upcoming"
-          component={Upcoming}
-          // options={{title: 'PAGER'}}
-        />
-        <Stack.Screen
-          name="Attended"
-          component={Attended}
-          // options={{title: 'PAGER'}}
-        />
-        <Stack.Screen
-          name="Create"
-          component={Create}
-          // options={{title: 'PAGER'}}
-        />
-      </Stack.Navigator>
-
-      // <Tab.Navigator
-      //   initialRouteName="Upcoming"
-      // >
-      //   <Tab.Screen
+      // <Stack.Navigator initialRouteName="Attended">
+      //   <Stack.Screen
       //     name="Upcoming"
       //     component={Upcoming}
+      //     // options={{title: 'PAGER'}}
       //   />
-      //   <Tab.Screen
+      //   <Stack.Screen
       //     name="Attended"
       //     component={Attended}
+      //     // options={{title: 'PAGER'}}
       //   />
-      //   <Tab.Screen
+      //   <Stack.Screen
       //     name="Create"
       //     component={Create}
+      //     // options={{title: 'PAGER'}}
       //   />
-      // </Tab.Navigator>
+      // </Stack.Navigator>
+
+      <Tab.Navigator
+        initialRouteName="Upcoming"
+        // screenOptions={({ route }) => ({
+        //   tabBarLabelStyle: { fontSize: 12 },
+        //   tabBarIndicatorStyle: { backgroundColor: route.name === 'Upcoming' ? '#B5179E' : route.name === 'Attended' ?'#7209B7' : route.name === 'Create' && '#4895EF' },
+        //   })}
+        screenOptions={{
+          tabBarLabelStyle: { fontSize: 12 },
+        }}
+        // screenOptions={{
+        //   tabBarLabelStyle: { fontSize: 12 },
+        //   tabBarIndicatorStyle: { backgroundColor: '#7209B7' },
+        // }}
+      >
+        <Tab.Screen
+          name="Upcoming"
+          component={Upcoming}
+          options={{
+            tabBarIndicatorStyle: { backgroundColor: '#B5179E' },
+          }}
+        />
+        <Tab.Screen
+          name="Attended"
+          component={Attended}
+          options={{
+              tabBarIndicatorStyle: { backgroundColor: '#7209B7' },
+          }}
+        />
+        <Tab.Screen
+          name="Create"
+          component={Create}
+          options={{
+              tabBarIndicatorStyle: { backgroundColor: '#4895EF' },
+          }}
+        />
+      </Tab.Navigator>
 
   )
 };
