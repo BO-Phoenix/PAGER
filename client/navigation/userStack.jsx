dd40f7de85336c53bcaab30e8e59820fc507f942
Charlie <charlesum@gmail.com>
2023-01-16 20:56:22 -0800

    fixed expo font installation issue

--- client/navigation/userStack.jsx
+++ client/navigation/userStack.jsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { React, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button } from 'react-native-elements';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import LoginScreen from '../screens/Login/Index.js';
 import HomePage from '../screens/HomePage/Index.js';
 import AllGroups from '../screens/AllGroups/Index.js';
@@ -11,8 +13,6 @@ import IndividualGroups from '../screens/IndividualGroups/Index.js';
 import { getEvents } from '../db/event';
 import globalStyles from '../globalStyles';
 import { useAuthentication } from '../utils/hooks/useAuthentication';
-import { Button } from 'react-native-elements';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const auth = getAuth();
 const Stack = createNativeStackNavigator();
@@ -45,11 +45,11 @@ export default function App() {
   const { user } = useAuthentication();
   return (
     <NavigationContainer>
-      <View id="header-container" style={styles.headerContainer}>
+      <SafeAreaView id="header-container" style={styles.headerContainer}>
         <Text id="app-header" style={styles.appHeader}>
           PAGER
         </Text>
-      </View>
+      </SafeAreaView>
       <Stack.Navigator initialRouteName="AllGroups">
         <Stack.Screen
           options={{ headerShown: false }}
@@ -68,6 +68,11 @@ export default function App() {
         />
       </Stack.Navigator>
       <Button title="Sign Out" style={globalStyles.button} onPress={() => auth.signOut()} />
+      <SafeAreaView id="header-container" style={styles.headerContainer}>
+        <Text id="app-header" style={styles.appHeader}>
+          PAGER
+        </Text>
+      </SafeAreaView>
     </NavigationContainer>
   );
 }
