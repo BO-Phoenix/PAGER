af60ec0e944a22b6b0d20385668f69daad01e9d9
Simon Buret De Longagne <buretdelongagnesimon@gmail.com>
2023-01-17 13:48:45 -0800

    added profile folder in screens and profile import in navigation/userstack

--- client/navigation/userStack.jsx
+++ client/navigation/userStack.jsx
@@ -10,6 +10,7 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { useFonts } from 'expo-font';
 import Loading from '../screens/Loading/Index.js';
 import LoginScreen from '../screens/Login/Index.js';
+import Profile from '../screens/Profile/Index.js';
 import HomePage from '../screens/HomePage/Index.js';
 import AllGroups from '../screens/AllGroups/Index.js';
 import IndividualGroups from '../screens/IndividualGroups/Index.js';
@@ -82,7 +83,12 @@ export default function App() {
           PAGER
         </Text>
       </SafeAreaView>
-      <Stack.Navigator initialRouteName="AllGroups">
+      <Stack.Navigator initialRouteName="Profile">
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Profile"
+          component={Profile}
+        />
         <Stack.Screen
           options={{ headerShown: false }}
           name="HomePage"
@@ -99,19 +105,35 @@ export default function App() {
           component={IndividualGroups}
         />
       </Stack.Navigator>
-      <Button title="Sign Out" style={globalStyles.button} onPress={() => auth.signOut()} />
+      <Button
+        title="Sign Out"
+        style={globalStyles.button}
+        onPress={() => auth.signOut()}
+      />
       <SafeAreaView id="header-container" style={styles.navContainer}>
         <View style={styles.navItem}>
-          <Text id="app-header" style={styles.navIcon}>ICON</Text>
-          <Text id="app-header" style={styles.navText}>PROFILE</Text>
+          <Text id="app-header" style={styles.navIcon}>
+            ICON
+          </Text>
+          <Text id="app-header" style={styles.navText}>
+            PROFILE
+          </Text>
         </View>
         <View style={styles.navItem}>
-          <Text id="app-header" style={styles.navIcon}>ICON</Text>
-          <Text id="app-header" style={styles.navText}>HOME</Text>
+          <Text id="app-header" style={styles.navIcon}>
+            ICON
+          </Text>
+          <Text id="app-header" style={styles.navText}>
+            HOME
+          </Text>
         </View>
         <View style={styles.navItem}>
-          <Text id="app-header" style={styles.navIcon}>ICON</Text>
-          <Text id="app-header" style={styles.navText}>GROUPS</Text>
+          <Text id="app-header" style={styles.navIcon}>
+            ICON
+          </Text>
+          <Text id="app-header" style={styles.navText}>
+            GROUPS
+          </Text>
         </View>
       </SafeAreaView>
     </NavigationContainer>
