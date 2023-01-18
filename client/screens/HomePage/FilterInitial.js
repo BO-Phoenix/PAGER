001c50506685387fb98f19709534fbd1298f82e0
Abigail LI <palmigloo@gmail.com>
2023-01-17 17:58:13 -0800

    populate data for select event

--- client/screens/HomePage/FilterInitial.js
+++ client/screens/HomePage/FilterInitial.js
@@ -1,10 +1,19 @@
 /* eslint-disable global-require */
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Pressable, CheckBox } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  CheckBox,
+  FlatList,
+} from 'react-native';
 import { useFonts } from 'expo-font';
 import Loading from '../Loading/Index.js';
 import globalStyles from '../../globalStyles';
 import emptyBox from '../../assets/box.png';
+import { getAllEvents } from '../../db/event.js';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,6 +23,8 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     overflow: 'scroll',
     backgroundColor: 'white',
+    // borderWidth: 3,
+    // borderColor: 'red',
   },
   headerText: {
     fontSize: 25,
@@ -22,12 +33,15 @@ const styles = StyleSheet.create({
   },
   filterContainer: {
     flex: 1,
+    width: '100%',
     flexDirection: 'row',
     flexWrap: 'wrap',
     alignItems: 'flex-start',
     justifyContent: 'center',
     margin: 10,
     paddingBottom: 200,
+    // borderWidth: 3,
+    // borderColor: 'green',
   },
   filterOptionContainer: {
     flexDirection: 'column',
@@ -39,8 +53,8 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   filterImage: {
-    width: 150,
-    height: 150,
+    width: 230,
+    height: 230,
     borderRadius: 10,
   },
   filterName: {
@@ -63,12 +77,22 @@ const styles = StyleSheet.create({
   },
 });
 
-const FilterInitial = () => {
+const Index = () => {
   const [fontLoaded] = useFonts({
     Poppins: require('../../assets/fonts/Poppins-Regular.ttf'),
     PoppinsBold: require('../../assets/fonts/Poppins-Bold.ttf'),
     Bebas: require('../../assets/fonts/BebasNeue-Regular.ttf'),
   });
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const events_list = await getAllEvents();
+      setEvents(events_list);
+    }
+    fetchData();
+  }, []);
 
   if (!fontLoaded) {
     return <Loading />;
@@ -78,27 +102,55 @@ const FilterInitial = () => {
     <View style={styles.container}>
       <Text style={styles.headerText}>SELECT EVENT</Text>
       <View style={styles.filterContainer}>
-        <View style={styles.filterOptionContainer}>
-          <Image style={styles.filterImage} source={require('../../assets/box.png')} />
+        <FlatList
+          data={events}
+          keyExtractor={(events) => events.id.toString()}
+          numColumns={2}
+          renderItem={({ item }) => (
+            // console.log('date is :',item.event_date)
+            <View style={styles.filterOptionContainer}>
+              <Image
+                style={styles.filterImage}
+                source={{ uri: item.event_image }}
+              />
+              <Text style={styles.filterName}>{item.event_name}</Text>
+              <Text style={styles.filterOptions}>{item.event_location}</Text>
+              <Text style={styles.filterOptions}>
+                {new Date(item.event_date.seconds * 1000).toDateString()}
+              </Text>
+            </View>
+          )}
+        />
+        {/* <View style={styles.filterOptionContainer}>
+          <Image
+            style={styles.filterImage}
+            source={require('../../assets/box.png')}
+          />
           <Text style={styles.filterName}>EVENT NAME</Text>
           <Text style={styles.filterOptions}>Location</Text>
           <Text style={styles.filterOptions}>Date</Text>
         </View>
         <View style={styles.filterOptionContainer}>
-          <Image style={styles.filterImage} source={require('../../assets/box.png')} />
+          <Image
+            style={styles.filterImage}
+            source={require('../../assets/box.png')}
+          />
           <Text style={styles.filterName}>EVENT NAME</Text>
           <Text style={styles.filterOptions}>Location</Text>
           <Text style={styles.filterOptions}>Date</Text>
         </View>
         <View style={styles.filterOptionContainer}>
-          <Image style={styles.filterImage} source={require('../../assets/box.png')} />
+          <Image
+            style={styles.filterImage}
+            source={require('../../assets/box.png')}
+          />
           <Text style={styles.filterName}>EVENT NAME</Text>
           <Text style={styles.filterOptions}>Location</Text>
           <Text style={styles.filterOptions}>Date</Text>
-        </View>
+        </View> */}
       </View>
     </View>
   );
 };
 
-export default FilterInitial;
+export default Index;
