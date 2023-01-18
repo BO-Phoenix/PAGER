bc08480ebd8eacbf956305906a1e07c4e1357405
joshuavilela <joshuavilela1@gmail.com>
2023-01-18 09:47:01 -0800

    added chat box

--- client/screens/IndividualGroups/Chat.js
+++ client/screens/IndividualGroups/Chat.js
@@ -1,12 +1,28 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback,
+} from 'react';
+import { useSelector } from 'react-redux';
+import {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot,
+} from 'firebase/firestore';
+import { signOut, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { db } from '../../firebase-config.js';
 import globalStyles from '../../globalStyles';
 
 const Stack = createStackNavigator();
+const authChat = getAuth();
 
 const styles = StyleSheet.create({
   container: {
@@ -30,6 +46,8 @@ const ChatStack = () => {
 };
 
 const Chat = ({ navigation }) => {
+  const { userId } = useSelector((state) => state.pagerData); // user_id global state
+
   return <ChatStack />;
 };
 
