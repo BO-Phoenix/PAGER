c1a15460cc511c830c117fee057d4244c7529a79
Charlie <charlesum@gmail.com>
2023-01-18 15:56:31 -0800

    update on swiping cards

--- client/screens/HomePage/Index.js
+++ client/screens/HomePage/Index.js
@@ -1,6 +1,4 @@
 /* eslint-disable global-require */
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { StyleSheet, Text, View, Image, Dimensions, Animated, PanResponder } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
@@ -14,89 +12,93 @@ import {
 } from 'react-native';
 import { useFonts } from 'expo-font';
 import Loading from '../Loading/Index.js';
-import SwipeCard from './SwipeCard.js';
-import SwipeFooter from './SwipeFooter.js';
 import globalStyles from '../../globalStyles';
 import emptyBox from '../../assets/box.png';
+import { getAllEvents } from '../../db/event.js';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingBottom: 10,
+    overflow: 'scroll',
+    backgroundColor: 'white',
+    // borderWidth: 3,
+    // borderColor: 'red',
+  },
+  headerText: {
+    fontSize: 25,
+    fontFamily: 'PoppinsBold',
+    margin: 10,
+  },
+  filterContainer: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    margin: 10,
+    paddingBottom: 200,
+  },
+  filterOptionContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    // borderWidth: 1,
+    // borderColor: 'black',
+    paddingHorizontal: 5,
+    marginBottom: 15,
+  },
+  filterImage: {
+    width: '90%',
+    height: 150,
+    borderRadius: 10,
+    // borderWidth: 1,
+    // borderColor: 'black',
+  },
+  filterName: {
+    fontFamily: 'PoppinsBold',
+    fontSize: 15,
+  },
+  filterOptions: {
+    fontFamily: 'Poppins',
+    fontSize: 15,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: '#F72585',
+    fontFamily: 'PoppinsBold',
+    color: 'white',
+    margin: 10,
   },
 });
 
-const Swipe = () => {
-  const groupsArray = [
-    { id: '1', source: require('../../assets/test.gif'), name: 'Name', description: 'blah b sdf dsf ds fds fds fds f ds fds f ds fds f ds fds f dsf ds  dsflah balhb albh' },
-    { id: '2', source: require('../../assets/test.gif'), name: 'Test', description: 'blah blah balhb albh' },
-    { id: '3', source: require('../../assets/test.gif'), name: 'Ug', description: 'blah blah balhb albh' },
-    { id: '4', source: require('../../assets/test.gif'), name: 'Ly', description: 'blah blah balhb albh' },
-    { id: '5', source: require('../../assets/box.png'), name: 'Boy', description: 'blah blah balhb albh' },
-  ];
-  const [groups, setGroups] = useState(groupsArray);
-
-
-  const screenWidth = (Dimensions.get('window').width) * 0.9;
-  const screenHeight = (Dimensions.get('window').height) * 0.75;
-  const outOfScreen = (Dimensions.get('window').width) + 0.5 * (Dimensions.get('window').width);
-  const swipe = useRef(new Animated.ValueXY()).current;
-  const tiltSign = useRef(new Animated.Value(1)).current;
-
-  useEffect(() => {
-    if (!groups.length) {
-      setGroups(groupsArray);
-    }
-  }, [groups.length]);
-
-  const panResponder = PanResponder.create({
-    onMoveShouldSetPanResponder: () => true,
-    onPanResponderMove: (_, { dx, dy, y0 }) => {
-      swipe.setValue({ x: dx, y: dy });
-      tiltSign.setValue(y0 > screenHeight / 2 ? 1 : -1);
-    },
-    onPanResponderRelease: (_, { dx, dy }) => {
-      const direction = Math.sign(dx);
-      const isActionActive = Math.abs(dx) > 100;
-
-      if (isActionActive) {
-        Animated.timing(swipe, {
-          duration: 200,
-          toValue: {
-            x: direction * outOfScreen,
-            y: dy,
-          },
-          useNativeDriver: true,
-        // eslint-disable-next-line no-use-before-define
-        }).start(removeTopCard);
-      } else {
-        Animated.spring(swipe, {
-          toValue: {
-            x: 0,
-            y: 0,
-          },
-          useNativeDriver: true,
-          friction: 5,
-        }).start();
-      }
-    },
-  });
-
-  const removeTopCard = useCallback(() => {
-    setGroups(({ navigation }prevState) => prevState.slice(1));
-    swipe.setValue({ x: 0, y: 0 });
-  }, [swipe]);
-
-
+const Index = ({ navigation }) => {
   const [fontLoaded] = useFonts({
     Poppins: require('../../assets/fonts/Poppins-Regular.ttf'),
     PoppinsBold: require('../../assets/fonts/Poppins-Bold.ttf'),
+    Bebas: require('../../assets/fonts/BebasNeue-Regular.ttf'),
   });
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const events_list = await getAllEvents();
+      setEvents(events_list);
+    }
+    fetchData();
+  }, []);
 
   if (!fontLoaded) {
     return <Loading />;
   }
-
 
   return (
     <View style={styles.container}>
@@ -109,10 +111,14 @@ const Swipe = () => {
           renderItem={({ item }) => (
             // console.log('date is :',item.event_date)
             <View style={styles.filterOptionContainer}>
-              <Image
-                style={styles.filterImage}
-                source={{ uri: item.event_image }}
-              />
+              <TouchableWithoutFeedback
+                onPress={() => navigation.navigate('SwipeCard', item)}
+              >
+                <Image
+                  style={styles.filterImage}
+                  source={{ uri: item.event_image }}
+                />
+              </TouchableWithoutFeedback>
               <Text style={styles.filterName}>{item.event_name}</Text>
               <Text style={styles.filterOptions}>{item.event_location}</Text>
               <Text style={styles.filterOptions}>
@@ -126,4 +132,4 @@ const Swipe = () => {
   );
 };
 
-export default Swipe;
+export default Index;
