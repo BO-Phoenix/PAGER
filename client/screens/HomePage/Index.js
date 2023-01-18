158a6e4378b417c670b1a3ec1f5caff79ac322ac
Charlie <charlesum@gmail.com>
2023-01-17 18:16:43 -0800

    updates

--- client/screens/HomePage/Index.js
+++ client/screens/HomePage/Index.js
@@ -1,10 +1,19 @@
 /* eslint-disable global-require */
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Pressable, CheckBox } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  CheckBox,
+  FlatList,
+} from 'react-native';
 import { useFonts } from 'expo-font';
 import Loading from '../Loading/Index.js';
 import globalStyles from '../../globalStyles';
 import emptyBox from '../../assets/box.png';
+import { getAllEvents } from '../../db/event.js';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,74 +23,76 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     overflow: 'scroll',
     backgroundColor: 'white',
-    // borderWidth: 1,
-    // borderColor: 'black',
+    // borderWidth: 3,
+    // borderColor: 'red',
   },
-  bodyContainerName: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    margin: 5,
-    // borderWidth: 1,
-    // borderColor: 'black',
-  },
-  headerImage: {
-    width: 50,
-    height: 50,
-    marginHorizontal: 5,
-  },
-  headerName: {
+  headerText: {
     fontSize: 25,
     fontFamily: 'PoppinsBold',
+    margin: 10,
   },
-  bodyContainerMember: {
+  filterContainer: {
+    flex: 1,
     width: '100%',
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    backgroundColor: 'white',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-  },
-  bodyContainerSection: {
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: 'white',
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-    marginTop: 5,
-  },
-  bodyContainerSchedule: {
-    width: '100%',
-    flexDirection: 'column',
+    flexWrap: 'wrap',
     alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    backgroundColor: 'white',
-    padding: 15,
+    justifyContent: 'center',
+    margin: 10,
+    paddingBottom: 200,
+    // borderWidth: 3,
+    // borderColor: 'green',
   },
-  textTitle: {
-    fontSize: 20,
+  filterOptionContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    // borderWidth: 1,
+    // borderColor: 'black',
+    marginBottom: 15,
+  },
+  filterImage: {
+    width: 230,
+    height: 230,
+    borderRadius: 10,
+  },
+  filterName: {
     fontFamily: 'PoppinsBold',
-  },
-  textDetailBold: {
     fontSize: 15,
-    fontFamily: 'PoppinsBold',
   },
-  textDetail: {
-    fontSize: 15,
+  filterOptions: {
     fontFamily: 'Poppins',
+    fontSize: 15,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: '#F72585',
+    fontFamily: 'PoppinsBold',
+    color: 'white',
+    margin: 10,
   },
 });
 
-const Schedule = () => {
+const Index = () => {
   const [fontLoaded] = useFonts({
     Poppins: require('../../assets/fonts/Poppins-Regular.ttf'),
     PoppinsBold: require('../../assets/fonts/Poppins-Bold.ttf'),
     Bebas: require('../../assets/fonts/BebasNeue-Regular.ttf'),
   });
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const events_list = await getAllEvents();
+      setEvents(events_list);
+    }
+    fetchData();
+  }, []);
 
   if (!fontLoaded) {
     return <Loading />;
@@ -89,34 +100,57 @@ const Schedule = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.bodyContainerSection}>
-        <Text style={styles.textDetail}>
-          {'< '}
-          BACK
-        </Text>
+      <Text style={styles.headerText}>SELECT EVENT</Text>
+      <View style={styles.filterContainer}>
+        <FlatList
+          data={events}
+          keyExtractor={(events) => events.id.toString()}
+          numColumns={2}
+          renderItem={({ item }) => (
+            // console.log('date is :',item.event_date)
+            <View style={styles.filterOptionContainer}>
+              <Image
+                style={styles.filterImage}
+                source={{ uri: item.event_image }}
+              />
+              <Text style={styles.filterName}>{item.event_name}</Text>
+              <Text style={styles.filterOptions}>{item.event_location}</Text>
+              <Text style={styles.filterOptions}>
+                {new Date(item.event_date.seconds * 1000).toDateString()}
+              </Text>
+            </View>
+          )}
+        />
+        {/* <View style={styles.filterOptionContainer}>
+          <Image
+            style={styles.filterImage}
+            source={require('../../assets/box.png')}
+          />
+          <Text style={styles.filterName}>EVENT NAME</Text>
+          <Text style={styles.filterOptions}>Location</Text>
+          <Text style={styles.filterOptions}>Date</Text>
+        </View>
+        <View style={styles.filterOptionContainer}>
+          <Image
+            style={styles.filterImage}
+            source={require('../../assets/box.png')}
+          />
+          <Text style={styles.filterName}>EVENT NAME</Text>
+          <Text style={styles.filterOptions}>Location</Text>
+          <Text style={styles.filterOptions}>Date</Text>
+        </View>
+        <View style={styles.filterOptionContainer}>
+          <Image
+            style={styles.filterImage}
+            source={require('../../assets/box.png')}
+          />
+          <Text style={styles.filterName}>EVENT NAME</Text>
+          <Text style={styles.filterOptions}>Location</Text>
+          <Text style={styles.filterOptions}>Date</Text>
+        </View> */}
       </View>
-      <View style={styles.bodyContainerName}>
-        <Image style={styles.headerImage} source={require('../../assets/box.png')} />
-        <Text style={styles.headerName}>Group Name</Text>
-      </View>
-      <View style={styles.bodyContainerSection}>
-        <Text style={styles.textDetailBold}>SCHEDULE</Text>
-      </View>
-      <View style={styles.bodyContainerSchedule}>
-        <Text style={styles.textDetailBold}>TIME</Text>
-        <Text style={styles.textDetail}>Schedule details will go here. Iflook like this.</Text>
-      </View>
-      <View style={styles.bodyContainerSchedule}>
-        <Text style={styles.textDetailBold}>TIME</Text>
-        <Text style={styles.textDetail}>Schedule details will go here.</Text>
-      </View>
-      <View style={styles.bodyContainerSchedule}>
-        <Text style={styles.textDetailBold}>TIME</Text>
-        <Text style={styles.textDetail}>Schedule details will go here.</Text>
-      </View>
-
     </View>
   );
 };
 
-export default Schedule;
+export default Index;
