08859683611b8ff1bac620411bf47a3d0ab6b383
Madeline King <madeline.king330@gmail.com>
2023-01-18 02:24:09 -0800

    continue building user group features

--- client/screens/AllGroups/Attended.js
+++ client/screens/AllGroups/Attended.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {
-  StyleSheet, Text, View, Button, TouchableOpacity,
+  StyleSheet, Text, View, Button, Image, TouchableOpacity,
 } from 'react-native';
+import Icon from 'react-native-ionicons';
 import { useSelector } from 'react-redux';
 import {
   getGroupsPerUser,
@@ -13,10 +14,49 @@ import {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    overflowY: 'scroll',
+  },
+  textHeader: {
+    fontSize: 28,
+    paddingTop: 15,
+  },
+  // underline: {
+  //   borderBottomWidth: '10',
+  //   borderBottomColor: 'black',
+  //   borderBottomStyle: 'solid',
+  // },
+  featureHeader: {
+    fontSize: 22,
+    paddingTop: 15,
+  },
+  groupName: {
+    fontSize: 22,
+  },
+  groupImg: {
+    height: 100,
+    width: 100,
+  },
+  renderGroupContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    padding: 10,
+    border: '2px solid grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  groupContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '90%',
+    padding: 10,
+    border: '2px solid grey',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
 
@@ -35,10 +75,19 @@ const Attended = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text>Attended</Text>
-      {attendedUserGroups.map(group => {
-        return <Text key={group.id}>{group.group_name}</Text>;
-      })}
+      <Text style={styles.textHeader}>GROUPS</Text>
+      <Text style={styles.featureHeader}>ATTENDED</Text>
+      <View style={styles.renderGroupContainer}>
+        {attendedUserGroups.map(group => {
+          return (
+            <View style={styles.groupContainer}>
+              <Image style={styles.groupImg} source={{ uri: group.group_image }} />
+              <Text style={styles.groupName} key={group.id}>{group.group_name}</Text>
+              <Icon name="arrow-dropright" size={30} />
+            </View>
+          );
+        })}
+      </View>
       {/* <TouchableOpacity
         title="Upcoming"
         onPress={() => navigation.navigate('Upcoming', { name: 'Upcoming' })}
